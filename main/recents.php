b6b8b2a5f6caa4e9bdf48dc97451b3485837725b
Francesca <matiraf@wit.edu>
2019-07-29 22:07:01 -0400

    testing sessions

--- main/recents.php
+++ main/recents.php
@@ -2,6 +2,7 @@
 
 // Initialize the session
 session_start();
+$_SESSION['id'];
 
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
